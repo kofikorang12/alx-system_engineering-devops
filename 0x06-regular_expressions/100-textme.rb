34d2be94d024d60420cede69e23179a8d49b0036
kofikorang12 <jbaah69@gmail.com>
2023-10-04 15:05:46 +0000

    regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-#sender = ARGV[0].match(/(?<=\[from:)[^\]]*(?=\])/)
-#receiver = ARGV[0].match(/(?<=\[to:)[^\]]*(?=\])/)
-#flags = ARGV[0].match(/(?<=\[flags:)[^\]]*(?=\])/)
+sender = ARGV[0].match(/(?<=\[from:)[^\]]*(?=\])/)
+receiver = ARGV[0].match(/(?<=\[to:)[^\]]*(?=\])/)
+flags = ARGV[0].match(/(?<=\[flags:)[^\]]*(?=\])/)
 
-#puts "%s,%s,%s" % [sender, receiver, flags]
+puts "%s,%s,%s" % [sender, receiver, flags]
