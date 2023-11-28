5d133d98e47fa17b4fa4a618001707a6c2061b23
Kofi korang <jbaah69@gmail.com>
2023-11-28 20:18:49 +0000

    peppet install ngix web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,18 @@
 # Install nginx server
 exec {'/usr/bin/env apt-get update':}
 exec {'/usr/bin/env apt-get install -y nginx':}
-exec {'/usr/bin/env mkdir -p /var/www/koeusiss/html':}
-exec {'/usr/bin/env echo "Holberton School" > /var/www/koeusiss/html/index.html':}
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/koeusiss/html/custom_404.html':}
+exec {'/usr/bin/env mkdir -p /var/www/kofikorang12/html':}
+exec {'/usr/bin/env echo "Hello World!" > /var/www/kofikorang12/html/index.html':}
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/kofikorang12/html/custom_404.html':}
 exec {'/usr/bin/env echo "server{
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/koeusiss/html;
+    root /var/www/kofikorang12/html;
     index index.html index.htm;
-    server_name koeusiss.tech;
+    server_name kofikorang12.tech;
     rewrite ^/redirect_me$ http://example.com permanent;
     error_page 404 /custom_404.html;
-}" > /etc/nginx/sites-available/koeusiss.tech':}
+}" > /etc/nginx/sites-available/kofikorang12.tech':}
 exec {'/usr/bin/env sudo rm /etc/nginx/sites-enabled/default':}
-exec {'/usr/bin/env sudo ln -s /etc/nginx/sites-available/koeusiss.tech /etc/nginx/sites-enabled/':}
+exec {'/usr/bin/env sudo ln -s /etc/nginx/sites-available/kofikorang12.tech /etc/nginx/sites-enabled/':}
 exec {'/usr/bin/env sudo service nginx restart':}
