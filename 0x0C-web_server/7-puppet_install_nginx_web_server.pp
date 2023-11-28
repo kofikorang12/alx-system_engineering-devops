1fe00da2499a1fae77e39cb6b06c6cf87ce5fd7c
Kofi korang <jbaah69@gmail.com>
2023-11-28 20:23:41 +0000

    peppet install ngix web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,6 @@
-# Install nginx server
-exec {'/usr/bin/env apt-get update':}
-exec {'/usr/bin/env apt-get install -y nginx':}
-exec {'/usr/bin/env mkdir -p /var/www/kofikorang12/html':}
-exec {'/usr/bin/env echo "Hello World!" > /var/www/kofikorang12/html/index.html':}
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/kofikorang12/html/custom_404.html':}
-exec {'/usr/bin/env echo "server{
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/kofikorang12/html;
-    index index.html index.htm;
-    server_name kofikorang12.tech;
-    rewrite ^/redirect_me$ http://example.com permanent;
-    error_page 404 /custom_404.html;
-}" > /etc/nginx/sites-available/kofikorang12.tech':}
-exec {'/usr/bin/env sudo rm /etc/nginx/sites-enabled/default':}
-exec {'/usr/bin/env sudo ln -s /etc/nginx/sites-available/kofikorang12.tech /etc/nginx/sites-enabled/':}
-exec {'/usr/bin/env sudo service nginx restart':}
+# Installs a Nginx server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/kofikorang12 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
